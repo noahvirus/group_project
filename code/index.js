5ab140d1f1005aef0baacbc1a06533ad8d7e8059
Matthew No <mano3845@colorado.edu>
2022-11-11 12:17:37 -0700

    added travel stuff in index.js

--- code/index.js
+++ code/index.js
@@ -1,22 +1,22 @@
-var express = require('express'); 
-var app = express(); 
+var express = require('express');
+var app = express();
 const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
 const session = require('express-session'); // allow us to save a user's data when they're browsing the website
 const bcrypt = require('bcrypt'); // for use with username and password
-    
-// Set EJS as templating engine 
-app.set('view engine', 'ejs'); 
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
 
 const dbConfig = { // database connection string - must be made to connect to a database
   host: 'db', // Running in the db container on the docker setup
   port: 5432, // other ports that are commonly known are 80, 8080, 8000, ... etc. You can find these with your terminal
-  database: process.env.POSTGRES_DB, 
+  database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
 };
 
-//create db with 
+//create db with
 const db = pgp(dbConfig);
 
 app.set('view engine', 'ejs');
@@ -39,8 +39,8 @@ app.use(
 app.get('/', (req, res) =>{
   res.redirect('/login'); //this will call the /anotherRoute route in the API
 });
-  
-app.get("/login", function(req, res) {  
+
+app.get("/login", function(req, res) {
   res.render("pages/login");
 });
 
@@ -49,7 +49,7 @@ app.post('/login', async (req, res) => {
   //the logic goes here
   const hash = await bcrypt.hash(req.body.password, 10);
   // insert into the database
-  var query = `SELECT password FROM users 
+  var query = `SELECT password FROM users
   WHERE username = $1;`
   db.any(query, [
     req.body.username,
@@ -61,12 +61,12 @@ app.post('/login', async (req, res) => {
     //   const match = await bcrypt.compare(req.body.password, user.password);
     //   console.log("after hash")
 
-      .then(async (user) => {   
+      .then(async (user) => {
           if (user.length == 0) {
             // then there was no password for username and they need to register
             console.log("Username not registered")
             res.redirect('/register')
-          }     
+          }
           else {
           const match = await bcrypt.compare(req.body.password, user[0].password);
 
@@ -100,6 +100,19 @@ app.get('/home', (req, res) => {
   res.render('pages/home');
 });
 
+app.get('/travel', (req, res) => {
+  res.render('pages/travel');
+});
+
+app.post('/travel', async (req, res) => {
+  //if not logged in,
+  //console.log("You are not logged in")
+  //res.redirect ('/login')
+  //else
+  //
+  }
+});
+
 app.get('/logout', (req, res) => {
   res.render("pages/login", {
     message: `Successfully logged out`,
@@ -122,9 +135,9 @@ app.post('/register', async (req, res) => {
   .catch(function (err) {
       res.redirect('/register');
   });
-  
+
 });
-    
+
 // Server setup
 app.listen(3000, function(req, res) {
   console.log("Connected on port:3000");
